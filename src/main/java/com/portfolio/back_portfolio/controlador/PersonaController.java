329f0a3ea4f6c86db213df1b5325f1953f8cebf5
Mauricio723 <salvadorvilches1972@gmail.com>
2023-02-02 02:41:00 -0300

    comenzando con Security

--- src/main/java/com/portfolio/back_portfolio/controlador/PersonaController.java
+++ src/main/java/com/portfolio/back_portfolio/controlador/PersonaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,47 @@ public class PersonaController {
 	public void creaarPersona(@RequestBody Persona personaCreada) {
 		
 		interfPersonaService.savePersona(personaCreada);
+	}
+	
+	@GetMapping("/personas/traer/{id}")
+	public Persona traerPersona(@PathVariable Long id) {		
+		return interfPersonaService.findPersona(id);
+	}
+	
+	@PutMapping("/personas/modificar/{id}")
+	public String modificarPersona (@PathVariable Long id, @RequestBody Persona personaParaModificar) {
+		Persona personaModificada = interfPersonaService.findPersona(id);
+		personaModificada.setNombre(personaParaModificar.getNombre());
+		personaModificada.setApellido(personaParaModificar.getApellido());
+		personaModificada.setOcupacion(personaParaModificar.getOcupacion());
+		personaModificada.setFecha_nacimiento(personaParaModificar.getFecha_nacimiento());
+		personaModificada.setDocumento(personaParaModificar.getDocumento());
+		personaModificada.setEmail(personaParaModificar.getEmail());
+		personaModificada.setAcerca_de(personaParaModificar.getAcerca_de());
+		personaModificada.setUrlFoto(personaParaModificar.getUrlFoto());
+		personaModificada.setUrlBanner(personaParaModificar.getUrlBanner());
+		
+		interfPersonaService.savePersona(personaModificada);
+		
+		return "Persona modificada con id: " + id;
+	}
+	
+	@PostMapping("/personas/prueba_crear")
+	public void crearPersonaPrueba() {
+		
+		Persona nuevaPersona = new Persona(
+				(long) 0, "Salvador Mauricio", 
+				"Vilches Jerez", 
+				"Estudiante", 
+				"30/05/1972", 
+				"92401924", 
+				"salvadorvilches1972@gmail.com", 
+				"Texto acerca de Mauricio", 
+				"url foto de Mauricio", 
+				"url de banner del portfolio");
+		
+		interfPersonaService.savePersona(nuevaPersona);
+		
 	}
 	
 	@DeleteMapping("/personas/eliminar/{id}")
--- src/main/java/com/portfolio/back_portfolio/modelo/Persona.java
+++ src/main/java/com/portfolio/back_portfolio/modelo/Persona.java
@@ -1,55 +1,47 @@
 package com.portfolio.back_portfolio.modelo;
 
-//import java.util.Date;
-//import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.OneToMany;
-//import javax.persistence.OneToOne;
+
 import javax.persistence.Table;
-//import javax.persistence.Temporal;
-//import javax.persistence.TemporalType;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
 @Entity
 @Table(name="personas")
 public class Persona {
 
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@GeneratedValue(strategy= GenerationType.IDENTITY)	
 	@Column(nullable=false, updatable=false)
 	private Long id;
-	
+		
 	@Column(length=50)
 	private String nombre;
+	
 	@Column(length=50)
 	private String apellido;
+	
 	@Column(length=50)
-	private String ocupacion;
-		
+	private String ocupacion;	
+	
 	@Column(length=50)
 	private String fecha_nacimiento;
 	
 	@Column(length=10)
-	private String documento;
-    
-	@Column(length=100)
-	private String email;
+	private String documento;    
 	
 	@Column(length=100)
-	private String acerca_de;
+	private String email;	
+		
+	@Column(length=100)
+	private String acerca_de;	
 	
 	@Column(length=100)
 	private String urlFoto;
+	
 	@Column(length=100)
 	private String urlBanner;
 		
@@ -73,5 +65,101 @@ public class Persona {
 	public Persona() {
 		super();
 	}
+
+	public Persona(Long id, String nombre, String apellido, String ocupacion, 
+			       String fecha_nacimiento, String documento,
+			       String email, String acerca_de, String urlFoto, String urlBanner) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.ocupacion = ocupacion;
+		this.fecha_nacimiento = fecha_nacimiento;
+		this.documento = documento;
+		this.email = email;		
+		this.acerca_de = acerca_de;
+		this.urlFoto = urlFoto;
+		this.urlBanner = urlBanner;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getOcupacion() {
+		return ocupacion;
+	}
+
+	public void setOcupacion(String ocupacion) {
+		this.ocupacion = ocupacion;
+	}
+
+	public String getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(String fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAcerca_de() {
+		return acerca_de;
+	}
+
+	public void setAcerca_de(String acerca_de) {
+		this.acerca_de = acerca_de;
+	}
+
+	public String getUrlFoto() {
+		return urlFoto;
+	}
+
+	public void setUrlFoto(String urlFoto) {
+		this.urlFoto = urlFoto;
+	}
+
+	public String getUrlBanner() {
+		return urlBanner;
+	}
+
+	public void setUrlBanner(String urlBanner) {
+		this.urlBanner = urlBanner;
+	}	
 	
 }
