bb01f7b25b57940ab4f79aee26b6557d42d075ca
Mauricio723 <salvadorvilches1972@gmail.com>
2023-03-24 06:35:29 -0300

    Corrección de SecurityMain

--- src/main/java/com/portfolio/back_portfolio/security/MainSecurity.java
+++ src/main/java/com/portfolio/back_portfolio/security/MainSecurity.java
@@ -5,15 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import com.portfolio.back_portfolio.security.jwt.JwtEntryPoint;
 import com.portfolio.back_portfolio.security.jwt.JwtTokenFilter;
 import com.portfolio.back_portfolio.security.service.UserDetailsImpl;
@@ -22,10 +22,11 @@ import com.portfolio.back_portfolio.security.service.UserDetailsImpl;
  * los métodos que no lleven esa anotación pueden acceder el admin y el user */
 /* Con @Preauthorized solo puede acceder el admin */
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MainSecurity {
+public class MainSecurity extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	UserDetailsImpl userDetailsServiceImpl;
@@ -45,27 +46,51 @@ public class MainSecurity {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-		http.cors().and().csrf().disable().authorizeRequests().antMatchers("/auth/**").permitAll().anyRequest()
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers("**").permitAll().anyRequest()
 				.authenticated().and().exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
 		http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
 
-		return http.build();
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-			throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
-	protected void configuracion(AuthenticationManagerBuilder auth) throws Exception {
-
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
 	}
 
+	/*
+	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
+	 * Exception {
+	 * http.cors().and().csrf().disable().authorizeRequests().antMatchers("/auth/**"
+	 * ).permitAll().anyRequest()
+	 * .authenticated().and().exceptionHandling().authenticationEntryPoint(
+	 * jwtEntryPoint).and()
+	 * .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	 * http.addFilterBefore(jwtTokenFilter(),
+	 * UsernamePasswordAuthenticationFilter.class);
+	 * return http.build(); }
+	 */
+
+	/*
+	 * @Bean public AuthenticationManager
+	 * authenticationManager(AuthenticationConfiguration
+	 * authenticationConfiguration) throws Exception { return
+	 * authenticationConfiguration.getAuthenticationManager(); }
+	 * protected void configuracion(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(
+	 * passwordEncoder()); }
+	 */
 }
